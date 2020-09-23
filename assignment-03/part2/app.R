b0cd9bc95dd050f1321cdfc2bcfcabacc5102e62
Tofik Mussa <tmussa@g.harvard.edu>
2020-09-23 12:54:26 -0400

    refactored to functions

--- assignment-03/part2/app.R
+++ assignment-03/part2/app.R
@@ -36,15 +36,22 @@ server <- function(session, input, output) {
   
   A <- matrix(sample(0:4, 9, replace = TRUE), nrow=3, ncol = 3)
   A
-  constCoef <- round(((4 * det(A)) %% 5), 2)
-  constCoef
-  lamCoef <- round(((4 * (-det(A[-2,-2]) - det(A[-3,-3]) - det(A[-1, -1]))) %% 5), 2)
-  lamCoef
   
-  lamSqCoef <- round(((4 * (A[1, 1] + A[2, 2] + A[3, 3])) %% 5), 2)
-  lamSqCoef
-  lamCubCoef <- round(((4 * -1) %% 5), 2)
-  lamCubCoef
+  calculateCoefficients <- function(){
+    
+    constCoef <- round(((4 * det(A)) %% 5), 2)
+    lamCoef <- round(((4 * (-det(A[-2,-2]) - det(A[-3,-3]) - det(A[-1, -1]))) %% 5), 2)
+    lamSqCoef <- round(((4 * (A[1, 1] + A[2, 2] + A[3, 3])) %% 5), 2)
+    lamCubCoef <- round(((4 * -1) %% 5), 2)
+    
+    coefs <- rbind(lamCubCoef, lamSqCoef, lamCoef, constCoef)
+    
+    return (coefs)
+  }
+  
+  coeficients <- calculateCoefficients()
+  
+  length(calculateCoefficients())
   
   findEigenValues <- function(){
     
@@ -52,8 +59,8 @@ server <- function(session, input, output) {
     
     for(x in 0:4){
       
-      temp <- round( (((lamCubCoef * (x ^ 3)) + (lamSqCoef * (x ^ 2)) 
-                     + (lamCoef * x) + constCoef)) %% 5, 2);
+      temp <- round( (((coeficients[1] * (x ^ 3)) + (coeficients[2] * (x ^ 2)) 
+                     + (coeficients[3] * x) + coeficients[4])) %% 5, 2);
       temp
       
       if(temp == 0){
@@ -66,10 +73,13 @@ server <- function(session, input, output) {
   
   findEigenValues();
   
+  length(findEigenValues());
+  
   iden <- diag(3)
   iden
   
-  ((lamCubCoef * (A%*%A%*%A)) + (lamSqCoef * (A%*%A)) + (lamCoef * A) + (constCoef * iden)) %% 5
+  ((coeficients[1] * (A%*%A%*%A)) + 
+      (coeficients[2] * (A%*%A)) + (coeficients[3] * A) + (coeficients[4] * iden)) %% 5
 }
 
 #Run the app
