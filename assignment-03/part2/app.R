8b443e2f85bb42da48bb692fc01829b9a719503b
Tofik Mussa <tmussa@g.harvard.edu>
2020-09-23 14:57:34 -0400

    added eigen vectors

--- assignment-03/part2/app.R
+++ assignment-03/part2/app.R
@@ -2,7 +2,8 @@
 library(shiny)
 library(shinydashboard)
 library(shinyWidgets)
-#source("jaxmat.R")   #for displaying mathematics
+library(pracma)
+# source("jaxmat.R")   #for displaying mathematics
 stylesheet <- tags$head(tags$style(HTML('
     .main-header .logo {
       font-family: "Georgia", Times, "Times New Roman", serif;
@@ -34,7 +35,9 @@ ui <- dashboardPage(header, sidebar, body, skin = "green") #other colors availab
 
 server <- function(session, input, output) {
   
-  A <- matrix(sample(0:4, 9, replace = TRUE), nrow=3, ncol = 3)
+  # A <- matrix(sample(0:4, 9, replace = TRUE), nrow=3, ncol = 3)
+  A <- matrix(c(1,0,0,0,2,-0,0,0,3), byrow = T, nrow=3)
+  A <- A %% 5
   A
   
   calculateCoefficients <- function(){
@@ -71,15 +74,57 @@ server <- function(session, input, output) {
     return (result);
   }
   
-  findEigenValues();
-  
   length(findEigenValues());
-  
+ 
   iden <- diag(3)
-  iden
+  
+  eigens <- findEigenValues();
+  
+  lamI1 <- eigens[1] * iden
+  lamI1
+  
+  lamI2 <- eigens[2] * iden
+  
+  lamI2
+  
+  lamI3 <- eigens[3] * iden
+  
+  lamI3
+  
+  prodEigen <- (lamI2 %*% lamI3) %% 5
+  
+  prodEigen <- cbind(prodEigen, c(0, 0, 0))
+  
+  prodEigen
+  
+  rref(prodEigen)
+  
+  prodEigen2 <- (lamI1 %*% lamI3) %% 5
+  
+  prodEigen2
+  
+  rref(prodEigen2)
+  
+  prodEigen2 <- cbind(prodEigen2, c(0, 0, 0))
+  
+  rref(prodEigen2)
+  
+  prodEigen3 <- (lamI1 %*% lamI2) %% 5
+  
+  prodEigen3 <- cbind(prodEigen3, c(0, 0, 0))
+  
+  rref(prodEigen3)
+  
+  mat <- matrix(c(3,4,3,2,1,3,2,1,4, 0, 0, 0), byrow = T, nrow=3)
+  mat
+  
+  rref(mat)
   
   ((coeficients[1] * (A%*%A%*%A)) + 
       (coeficients[2] * (A%*%A)) + (coeficients[3] * A) + (coeficients[4] * iden)) %% 5
+  
+  
+  
 }
 
 #Run the app
