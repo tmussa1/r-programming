7745b209f33db5101bda44994869f95a2793bc79
Tofik Mussa <tofahub@gmail.com>
2020-10-13 19:31:55 -0400

    part 2 working

--- assignment-06/midterm-project/app.R
+++ assignment-06/midterm-project/app.R
@@ -12,7 +12,7 @@ sidebar <- dashboardSidebar(disable = TRUE)
 
 body <- dashboardBody(
   navbarPage(
-  "Select a Continent/Country",
+  "Select a Continent & Country",
   tabPanel(
     fluidPage(fluidRow(
       column(
@@ -28,7 +28,8 @@ body <- dashboardBody(
         width = 6
       )
     )),
-    plotOutput("covid_chart")
+    plotOutput("covid_chart"),
+    plotOutput("covid_chart_deaths")
   ),
   collapsible = TRUE
 ))
@@ -41,22 +42,47 @@ server <- function(session, input, output) {
   covid_data <- as_tibble(read_excel("data/covid_data.xlsx"))
   countries <- unique(covid_data %>% pull(countriesAndTerritories))
   continents <- unique(covid_data %>% pull(continentExp))
+  covid_data_per_country_per_month <- covid_data %>% group_by(countriesAndTerritories, month)
+  covid_data_per_country_per_month <- covid_data_per_country_per_month %>% summarise(
+    cases = sum(cases),
+    deaths = sum(deaths)
+  )
   
   updateSelectInput(session,
                     "selected_continent",
                     choices = continents)
   
   observeEvent(input$selected_continent,
-               {
-                 countries_in_continent <- covid_data %>%
-                   filter(continentExp == input$selected_continent) %>%
-                   pull(countriesAndTerritories)
+  {
+   countries_in_continent <- covid_data %>%
+     filter(continentExp == input$selected_continent) %>%
+     pull(countriesAndTerritories)
 
-                 updateSelectInput(session,
-                                   "selected_country",
-                                   choices = countries_in_continent)
-
+   updateSelectInput(session,
+                     "selected_country",
+                     choices = countries_in_continent)
   })
+  
+ 
+  observeEvent(input$selected_country,{
+    
+    months <- c("Jaunary", "February", "March", "April", "May", 
+                "June", "July", "August", "September", "October")
+    
+    selectedCountry <- covid_data_per_country_per_month %>% 
+      filter(countriesAndTerritories == input$selected_country)
+
+    output$covid_chart <- renderPlot(
+      ggplot(data = selectedCountry,
+             aes(x=factor(month), y=cases, fill=cases)) +
+        geom_bar(stat="identity", position = "dodge"))
+   
+    output$covid_chart_deaths <- renderPlot(
+      ggplot(data = selectedCountry,
+             aes(x=factor(month), y=deaths, fill=deaths)) +
+        geom_bar(stat="identity", position = "dodge"))
+  })
+
 }
 
 shinyApp(ui = ui, server = server)
